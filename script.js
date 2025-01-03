eb512f77f32fcc4c6b18938cc2c4e106ac852351
akash <musaddekurakash@gmail.com>
2025-01-03 22:41:58 +0600

    data showing condition added

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  // Fetch data from the API
   fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-    .then((response) => response.json())
-    .then((data) => displayCardData(data.drinks))
-    .catch((error) => console.log(error));
+        .then(response => response.json())
+        .then(data => displayCardData(data.drinks))
+        .catch(error => console.log(error));
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const inputField = document.getElementById("inputField");
+    const query = inputField.value.trim();
+    if (query) {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.drinks) {
+                    displayCardData(data.drinks);
+                } else {
+                    displayNotFound();
+                }
+            })
+            .catch(error => console.log(error));
+    } else {
+        displayNotFound();
+    }
+});
 });
 
 let totalCart = 0;
@@ -39,12 +62,30 @@ function displayCardData(drinks) {
     const cartDetails = card.querySelector(".cartDetails");
     cartDetails.addEventListener("click", (event) => {
       event.preventDefault();
-      showDetails(drink);
+      drinkDetails(drink.idDrink);
     });
   });
 }
 
+function displayNotFound() {
+  const cardContainer = document.querySelector(".col-md-8 .row");
+  cardContainer.innerHTML = `
+      <div class="col-md-12">
+          <div class="alert alert-danger" role="alert">
+              No drinks found.
+          </div>
+      </div>
+  `;
+}
+
+
 function addToCart(drink) {
+
+  if (totalCart >= 8) {
+    alert("You can only add up to 8 items to the cart.");
+    return;
+}
+
   totalCart += 1;
   const cartCountElement = document.getElementById("cartCount");
   if (cartCountElement) {
@@ -55,18 +96,27 @@ function addToCart(drink) {
   const row = document.createElement("tr");
   row.innerHTML = `
     <th scope="row">${totalCart}</th>
-    <td class="w-25"><img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" class="img-fluid" style="max-width: 50px;"></td>
+    <td><img src="${drink.strDrinkThumb}" alt="${drink.strDrink}" class="img-fluid" style="max-width: 50px;"></td>
     <td>${drink.strDrink}</td>
   `;
   cartTableBody.appendChild(row);
 }
 
+function drinkDetails(drinkId) {
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
+    .then((response) => response.json())
+    .then((data) => showDetails(data.drinks[0]))
+    .catch((error) => console.log(error));
+}
+
 function showDetails(drink) {
-  // alert(drink.strInstructions);
+  console.log(drink);
   const modalBody = document.querySelector("#drinkDetailsModal .modal-body");
   modalBody.innerHTML = `
    <h5>${drink.strDrink}</h5>
-    <img src="${drink.strDrinkThumb}" class="img-fluid" alt="${drink.strDrink}">
+    <img src="${drink.strDrinkThumb}" class="img-fluid" alt="${drink.strDrink}" style="max-width: 200px">
+    <h5>Category: ${drink.strCategory}</h5>
+    <h5>Alcoholic: ${drink.strAlcoholic}</h5>
     <p>${drink.strInstructions}</p>
 `;
 
